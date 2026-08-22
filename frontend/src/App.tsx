0062c49d44aa57c422189e2aea7a28b8974c2399
juanm-0 <manueljuan2492@gmail.com>
2026-08-22 14:43:29 -0500

    feat(frontend): TanStack Router and Query Infra

--- frontend/src/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div>Shared Recipe Application</div>
-}
-
-export default App
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen'
+
+const queryClient = new QueryClient()
+
+const router = createRouter({ routeTree })
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
