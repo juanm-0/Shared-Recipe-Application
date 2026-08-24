deadd3095c38fcc82be13583a9380dc9168f067e
juanm-0 <manueljuan2492@gmail.com>
2026-08-23 23:55:13 -0500

    feat(frontend): setup tailwindcss

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
@@ -9,6 +10,7 @@ export default defineConfig({
       autoCodeSplitting: true,
     }),
     react(),
+    tailwindcss(),
   ],
   server: {
     host: true,
